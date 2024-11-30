af8c79b284a87b19f93c33c1556a4be4eb2e2c7b
Diego Caucota <diegocaucota@gmail.com>
2024-11-29 23:48:46 -0300

    add: logout administrador

--- administrador/index.php
+++ administrador/index.php
@@ -1,3 +1,18 @@
+<?php 
+session_start();
+  if($_POST) {
+    if($_POST['usuario'] == 'admin' && $_POST['password'] == 'admin') {
+      
+      $_SESSION['usuario'] = 'ok';
+      $_SESSION['nombreUsuario'] = 'Administrador';
+      
+      header('Location: inicio.php');
+    }else {
+      $mensaje = 'Usuario o Password incorrectos';
+    }
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,22 +30,34 @@
       <div class="col-md-4">
         <br/><br/><br/>
         <div class="card">
-          <form>
-          <div class = "form-group">
-          <label for="txtUsuario">Usuario</label>
-          <input type="text" class="form-control" id="txtUsuario" placeholder="Ingrese su Usuario">
+          <div class="card-body">
+            Login
           </div>
-          <div class="form-group">
-          <label for="txtPassword">Password</label>
-          <input type="password" class="form-control" id="txtPassword" placeholder="Ingrese su contraseña">
-          </div>
-         
-          <button type="submit" class="btn btn-primary">Ingresar al Adminisatrador</button>
-          </form>
-          
-          
+          <div class="card-body"> 
+            
+          <?php if(isset($mensaje)) { ?>
+            <div class="alert alert-danger" role="alert">
+              <?php echo $mensaje; ?>
+            </div>
+            <?php } ?>
+            
+            <form method="POST">
+            
+            <div class = "form-group">
+              <label>Usuario</label>
+              <input type="text" class="form-control" name="usuario" placeholder="Ingrese su Usuario">
+            </div>
+            
+            <div class="form-group">
+              <label for="txtPassword">Password</label>
+              <input type="password" class="form-control" name="password" placeholder="Ingrese su password">
+            </div>           
+            
+            <button type="submit" class="btn btn-primary">Ingresar al Adminisatrador</button>
+            
+            </form>
+          </div>          
         </div>
-
       </div>    
     </div>
   </div>
